f47299ab65b4a61b078b878ee7ba8d9947e27578
Richard Eames <reames@asymmetricventures.com>
2012-05-07 14:55:21 -0600

    - Fix logging imports - Fix bad ending tag

--- asymmetricbase/logging/__init__.py
+++ asymmetricbase/logging/__init__.py
@@ -1,15 +1,17 @@
 import logging
-from django.conf import settings
+
+from django.utils.functional import lazy
 
 class NullHandler(logging.Handler):
 	def emit(self, record):
 		pass
 
 def init_logger():
+	from django.conf import settings
 	logger_name = getattr(settings, 'ASYM_LOGGER', 'asymm_logger')
 	
 	_logger = logging.getLogger(logger_name)
-	logger.addHandler(NullHandler())
+	_logger.addHandler(NullHandler())
 	return _logger
 
-logger = init_logger()
+logger = lazy(init_logger, logging.Logger)
