5d4a06d6dc1488a14580ef5f450a4852c6ee6983
Richard Eames <reames@asymmetricventures.com>
2012-05-07 13:36:28 -0600

    - Remove less generic jinja filters - Fix up logger to be more easily modified

--- asymmetricbase/jinja/__init__.py
+++ asymmetricbase/jinja/__init__.py
@@ -11,8 +11,6 @@ from django.utils import timezone
 from django.conf import settings
 
 import jinja2
-from jinja2.filters import evalcontextfilter
-from jinja2._markupsafe import Markup
 from jinja2.ext import WithExtension
 
 from asymmetricbase.jinja.tags.csrf_token import CSRFTokenExtension
@@ -40,7 +38,7 @@ class UndefinedVar(jinja2.Undefined):
 		return EmptyIter()
 		
 	def __html__(self):
-		return '%NONE%'
+		return u'%NONE%'
 	
 	def __getattribute__(self, name, *args, **kwargs):
 			
@@ -75,31 +73,6 @@ def jinja_getdate():
 def jinja_date_filter(d, fmt = "%d/%b/%y %I:%M%p"):
 	return d.strftime(fmt)
 
-def jinja_month(d):
-	if isinstance(d, datetime.datetime):
-		return d.strftime('%b')
-	else:
-		return datetime.date(2000, d, 1).strftime('%b')
-
-def jinja_percent(d):
-	return '{:.0f}'.format(d * 100)
-
-@evalcontextfilter
-def jinja_nl2br(ctx, text):
-	result = unicode(text).replace('\n', '<br />')
-	
-	if ctx.autoescape:
-		result = Markup(result)
-	
-	return result
-
-@evalcontextfilter
-def br2nl(ctx, value):
-	result = unicode(value).replace('<br>', '\n').replace('<br/>', '\n').replace('<br />', '\n')
-	
-	if ctx.autoescape:
-		result = Markup(result)
-	return result
 
 jinja_env.globals.update({
 	'url' : jinja_url,
@@ -110,9 +83,4 @@ jinja_env.filters.update({
 	'date' : jinja_date_filter,
 	'floatformat' : floatformat,
 	'yesno' : yesno,
-	'br2nl' : br2nl,
-	'nl2br' : jinja_nl2br,
-	
-	'month' : jinja_month,
-	'percent' : jinja_percent,
 })
--- asymmetricbase/logging/__init__.py
+++ asymmetricbase/logging/__init__.py
@@ -1,4 +1,15 @@
 import logging
 from django.conf import settings
 
-logger = logging.getLogger(getattr(settings, 'ASYM_LOGGER',  'asymm_logger'))
+class NullHandler(logging.Handler):
+	def emit(self, record):
+		pass
+
+def init_logger():
+	logger_name = getattr(settings, 'ASYM_LOGGER', 'asymm_logger')
+	
+	_logger = logging.getLogger(logger_name)
+	logger.addHandler(NullHandler())
+	return _logger
+
+logger = init_logger()
