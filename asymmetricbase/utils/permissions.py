ab8ffabcf1267c7f799c203f99768e87bd2df19d
Richard Eames <reames@asymmetricventures.com>
2012-12-14 12:53:30 -0700

    - Make create_codename more robust

--- asymmetricbase/utils/permissions.py
+++ asymmetricbase/utils/permissions.py
@@ -1,15 +1,21 @@
 from django.contrib.contenttypes.models import ContentType
+from asymmetricbase.utils.cached_function import cached_function
 
+@cached_function
 def default_content_type():
 	return ContentType.objects.get(model = 'user')
 
 def default_content_type_appname():
 	return 'auth'
 
-def create_codename(module_path, cls_name):
+def create_codename(module_path, cls_name, suffix = ''):
+	# Remove the repeated parts of the path
+	module_path = module_path.replace('.views', '').replace('project.', '')
+	
 	# return only the first 100 characters, since that's
 	# all that fits in the DB
-	return 'view_{}.{}'.format(
-		module_path[8:],
-		cls_name
+	return 'view_{}.{}{}'.format(
+		module_path,
+		cls_name,
+		suffix
 	)[:100]
