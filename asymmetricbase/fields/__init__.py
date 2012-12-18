23ceb7e71add0b35c8141f806f75e6c4c532fc97
Richard Eames <reames@asymmetricventures.com>
2012-12-18 15:15:16 -0700

    - Added Char fields with semantic names for different max_lengths

--- asymmetricbase/fields/__init__.py
+++ asymmetricbase/fields/__init__.py
@@ -2,3 +2,5 @@ from dollarfield import DollarField, ZERO_DOLLARS
 from enumfield import EnumField, EnumFormField
 from extended_modelmultiplechoicefield import ExtendedModelMultipleChoiceField
 from rangefield import IntegerRangeField
+from textfields import COMMENT_LENGTH, LONG_MESSAGE_LENGTH, LONG_NAME_LENGTH, SHORT_MESSAGE_LENGTH, SHORT_NAME_LENGTH, \
+	CommentField, LongMessageField, LongNameField, ShortMessageField, ShortNameField
