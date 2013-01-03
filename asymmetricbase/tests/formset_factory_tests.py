af4d79997f8c9a9c0574bb08033c9bdad476848c
Aaron Krebs <akrebs@asymmetricventures.com>
2013-01-03 13:05:17 -0700

    - More comprehensive tests for InlineFormsetFactoryFactory

--- asymmetricbase/tests/formset_factory_tests.py
+++ asymmetricbase/tests/formset_factory_tests.py
@@ -5,6 +5,8 @@ from asymmetricbase.forms.formset_factoryfactory import FormSetFactoryFactory, \
 	ModelFormSetFactoryFactory, InlineFormSetFactoryFactory
 from asymmetricbase import forms
 from asymmetricbase.tests.models import TestModel, FKTestModel
+#FKFKTestModel
+#from django.forms.models import BaseInlineFormSet
 
 class TestForm(forms.Form):
 	field1 = forms.BooleanField(required = False)
@@ -17,6 +19,26 @@ class TestModelForm(forms.ModelForm):
 class TestInlineForm(forms.ModelForm):
 	class Meta(object):
 		model = FKTestModel
+
+#class TestNestedInlineFormSet(BaseInlineFormSet):
+#	"""
+#	InlineFormSets can be nested by overriding the
+#	add_fields method and saving subforms as an attribute
+#	on the form.
+#	stackoverflow.com/questions/7648368/django-inline-formset-setup
+#	"""
+#	def add_fields(self, form, index):
+#		# allow the superclass to add fields as usual
+#		super(TestNestedInlineFormSet, self).add_fields(form, index)
+#		
+#		try:
+#			instance = self.get_queryset()[index]
+#		except IndexError:
+#			instance = None
+#		pk_value = hash(form.prefix)
+#		
+#		# store the formset on the form
+#		form.nested = []
 
 class FormsetFactoryFactoryTests(BaseTestCaseWithModels):
 	
@@ -69,19 +91,99 @@ class FormsetFactoryFactoryTests(BaseTestCaseWithModels):
 		self.assertEqual(form2.cleaned_data['field1'], 4)
 		self.assertEqual(form2.cleaned_data['field2'], 'World')
 	
-	def test_create_inlineformset1(self):
+	def test_instantiate_inlineformset1(self):
+		"""
+		Just make sure there are no exceptions.
+		"""
+		# create top level TestModel
 		test_model = TestModel.objects.create(field1 = True, field2 = 1)
-		fk_test_model1 = FKTestModel.objects.create(test_model = test_model)
-		fk_test_model2 = FKTestModel.objects.create(test_model = test_model)
-		
-#		request = self.factory.post('/', {
-#			'form-TOTAL_FORMS' : '2',
-#			'form-INITIAL_FORMS' : '0',
-#			'form-MAX_NUM_FORMS' : '',
-#			'form-0-field1' : '1',
-#			'form-0-field2' : 'Hello',
-#			'form-1-field1' : '4',
-#			'form-1-field2' : 'World',
-#		})
+		# create two FKTestModel instances with ForeignKey to test_model
+		fk_test_model1 = FKTestModel.objects.create( #@UnusedVariable
+			test_model = test_model,
+			field1 = True,
+			field2 = 1,
+		)
+		fk_test_model2 = FKTestModel.objects.create( #@UnusedVariable
+			test_model = test_model,
+			field1 = True,
+			field2 = 1,
+		)
 		request = self.factory.get('/')
+		form_instance = InlineFormSetFactoryFactory(TestModel, FKTestModel, instance = test_model, extra = 0)(request) #@UnusedVariable
+	
+	def test_create_inlineformset1(self):
+		# create top level TestModel
+		test_model = TestModel.objects.create(field1 = True, field2 = 1)
+		request = self.factory.post('/', {
+			'fktestmodel_set-TOTAL_FORMS' : '2',
+			'fktestmodel_set-INITIAL_FORMS' : '0',
+			'fktestmodel_set-MAX_NUM_FORMS' : '',
+			'fktestmodel_set-0-test_model' : test_model.id,
+			'fktestmodel_set-0-field1' : '1',
+			'fktestmodel_set-0-field2' : 'Hello',
+			'fktestmodel_set-1-test_model' : test_model.id,
+			'fktestmodel_set-1-field1' : '4',
+			'fktestmodel_set-1-field2' : 'World',
+		})
 		form_instance = InlineFormSetFactoryFactory(TestModel, FKTestModel, instance = test_model, extra = 0)(request)
+		
+		self.assertTrue(form_instance.is_valid())
+		
+		form1 = form_instance[0]
+		form2 = form_instance[1]
+		
+		self.assertEqual(form1.cleaned_data['test_model'], test_model)
+		self.assertEqual(form1.cleaned_data['field1'], 1)
+		self.assertEqual(form1.cleaned_data['field2'], 'Hello')
+		
+		self.assertEqual(form2.cleaned_data['test_model'], test_model)
+		self.assertEqual(form2.cleaned_data['field1'], 4)
+		self.assertEqual(form2.cleaned_data['field2'], 'World')
+		
+#	def test_create_nested_inlineformset(self):
+#		# create top level TestModel
+#		test_model = TestModel.objects.create(field1 = True, field2 = 1)
+#		# create two FKTestModel instances with ForeignKey to test_model
+#		fk_test_model1 = FKTestModel.objects.create(
+#			test_model = test_model,
+#			field1 = True,
+#			field2 = 1,
+#		)
+#		fk_test_model2 = FKTestModel.objects.create(
+#			test_model = test_model,
+#			field1 = True,
+#			field2 = 1,
+#		)
+#		# for each of these, create two FKFKTestModel instances
+#		fk_fk_test_model1 = FKFKTestModel.objects.create(
+#			fk_test_model = fk_test_model1,
+#			field1 = True,
+#			field2 = 1,
+#		)
+#		fk_fk_test_model2 = FKFKTestModel.objects.create(
+#			fk_test_model = fk_test_model1,
+#			field1 = True,
+#			field2 = 1,
+#		)
+#		fk_fk_test_model3 = FKFKTestModel.objects.create(
+#			fk_test_model = fk_test_model2,
+#			field1 = True,
+#			field2 = 1,
+#		)
+#		fk_fk_test_model4 = FKFKTestModel.objects.create(
+#			fk_test_model = fk_test_model2,
+#			field1 = True,
+#			field2 = 1,
+#		)
+##		request = self.factory.post('/', {
+##			'form-TOTAL_FORMS' : '2',
+##			'form-INITIAL_FORMS' : '0',
+##			'form-MAX_NUM_FORMS' : '',
+##			'form-0-field1' : '1',
+##			'form-0-field2' : 'Hello',
+##			'form-1-field1' : '4',
+##			'form-1-field2' : 'World',
+##		})
+#		request = self.factory.get('/')
+#		form_instance = InlineFormSetFactoryFactory(TestModel, FKTestModel, instance = test_model, extra = 0)(request)
+#		
