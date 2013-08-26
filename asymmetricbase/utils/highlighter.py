dea391cb60075fabfdeedfb354f39e4f6829f196
Aaron Krebs <akrebs@asymmetricventures.com>
2013-08-26 11:17:33 -0600

    Fix unicode errors in highlighter.

--- asymmetricbase/utils/highlighter.py
+++ asymmetricbase/utils/highlighter.py
@@ -29,14 +29,14 @@ def highlighter_wrapper(*queries):
 		if not query:
 			continue
 		rx = "({})".format(re.escape(query))
-		matchers.append(re.compile(rx, re.I))
+		matchers.append(re.compile(rx, re.I|re.UNICODE))
 	
 	def highlighter(phrase):
 		phrase = unicode(phrase)
+		phrase = unicode(escape(phrase))
 		for matcher in matchers:
 			match = matcher.search(phrase)
 			if match is not None:
-				phrase = str(escape(phrase))
-				phrase = Markup(matcher.sub(r"<strong>\1</strong>", phrase))
-		return phrase
+				phrase = matcher.sub(r"<strong>\1</strong>", phrase)
+		return Markup(phrase)
 	return highlighter
