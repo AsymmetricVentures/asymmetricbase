9715dc186c9262901aafbbb78404691f3cf3f3fe
Aaron Krebs <akrebs@asymmetricventures.com>
2014-01-07 17:12:37 -0700

    Changed clean_logs command to also archive AuditEntry and TraceEntry.
    Objects are serialized to json file.

--- asymmetricbase/management/commands/clean_logs.py
+++ asymmetricbase/management/commands/clean_logs.py
@@ -22,14 +22,51 @@ from datetime import timedelta
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from django.conf import settings
+from django.core import serializers
 
 class Command(BaseCommand):
 	
 	def handle(self, *args, **options):
-		from asymmetricbase.models import LogEntry
+		"""
+		Archive old log entries. Provide path as first argument to change
+		location of output file.
+		"""
+		from asymmetricbase.models import LogEntry, TraceEntry, AuditEntry
 		
-		cutoff = timezone.now() - timedelta(days = getattr(settings, 'ASYM_KEEP_LOGS',  4)) # only keep 4 days worth
+		path = args[0] if len(args) > 0 else './'
 		
-		LogEntry.objects.filter(
-			date_created__lt = cutoff
-		).delete()
+		keep_days = getattr(settings, 'ASYM_KEEP_LOGS',  4) # only keep 4 days worth
+		cutoff = timezone.now() - timedelta(days = keep_days)
+		JSONSerializer = serializers.get_serializer('json')
+		json_serializer = JSONSerializer()
+		
+		# get filename prefixes
+		logs_prefix = getattr(settings, 'ASYM_LOGENTRY_ARCHIVE_FILE_PREFIX', 'log')
+		traces_prefix = getattr(settings, 'ASYM_TRACEENTRY_ARCHIVE_FILE_PREFIX', 'trace')
+		audits_prefix = getattr(settings, 'ASYM_AUDITENTRY_ARCHIVE_FILE_PREFIX', 'audit')
+		
+		# get querysets for each log type
+		logs = LogEntry.objects.filter(date_created__lt = cutoff)
+		traces = TraceEntry.objects.filter(date_created__lt = cutoff)
+		audits = AuditEntry.objects.filter(time_stamp__lt = cutoff)
+		
+		# combine into list
+		to_archive = [
+			{'prefix': logs_prefix, 'qs': logs},
+			{'prefix': traces_prefix, 'qs': traces},
+			{'prefix': audits_prefix, 'qs': audits},
+		]
+		
+		for parms in to_archive:
+			# write objects to file
+			filename = '{}{}_archive_{}_keep_{}_days.json'.format(
+				path,
+				parms['prefix'],
+				timezone.now(),
+				keep_days,
+			)
+			with open(filename, 'w') as out:
+				json_serializer.serialize(parms['qs'], stream = out)
+			
+			# delete objects from DB
+			parms['qs'].delete()
