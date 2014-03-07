16b4e1e8f2551a1710f43d1f31f32f100a3accb4
Richard Eames <reames@asymmetricventures.com>
2014-03-07 13:08:50 -0700

    - Don't emit duplicate queries - Use the built-in functions to generate the sql - Don't actually issue the commands, just print them

--- asymmetricbase/management/commands/recreate_fkeys.py
+++ asymmetricbase/management/commands/recreate_fkeys.py
@@ -20,8 +20,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from django.core.management.base import BaseCommand
 from django.db.models import get_models
 from django.db import connections#, transaction
-#from django.core.management.color import color_style
-from django.db.backends.util import truncate_name
+from django.core.management.color import no_style
 
 from south.db.generic import DatabaseOperations
 
@@ -54,36 +53,26 @@ class Command(BaseCommand):
 					
 					pending_references.setdefault(f.rel.to, []).append((model, f))
 		
-		drops = []
-		adds = []
+		drops = set()
+		adds = set()
+		
 		
 		for model in get_models():
 			table_name = model._meta.db_table
 			
-			for col, constraints in d.lookup_constraint(connection_name, table_name):
+			for _, constraints in d.lookup_constraint(connection_name, table_name):
 				for kind, cname in constraints:
 					if kind == 'FOREIGN KEY':
-						drops.append(d.delete_foreign_key_sql % {'table' : qn(table_name), 'constraint' : qn(cname)} + ';')
+						drops.add(d.delete_foreign_key_sql % {'table' : qn(table_name), 'constraint' : qn(cname)} + ';')
 			
 			opts = model._meta
 			
-			if model in pending_references:
-				for rel_class, f in pending_references[model]:
-					rel_opts = rel_class._meta
-					r_table = rel_opts.db_table
-					r_col = f.column
-					#table = opts.db_table
-					col = opts.get_field(f.rel.field_name).column
-					
-					r_name = '{}_refs_{}_{}_fkey'.format(r_col, table_name, col)
-					r_name = truncate_name(r_name, connection.ops.max_name_length())
-					
-					adds.append('ALTER TABLE {} ADD CONSTRAINT {} FOREIGN KEY ({}) REFERENCES {} ({}){};'.format(qn(r_table), qn(r_name), qn(r_col), qn(table_name), qn(col), connection.ops.deferrable_sql()))
-					
-				del pending_references[model]
+			adds.update(set(connection.creation.sql_for_pending_references(model, no_style(), pending_references)))
+			
 		
-		for sql in drops + adds:
-			if int(options['verbosity']) > 1:
-				print(sql)
-			connection.cursor().execute(sql)
+		for sql in drops:
+			print(sql)
+		
+		for sql in adds:
+			print(sql)
 		
