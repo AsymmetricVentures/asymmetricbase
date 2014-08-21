6289a613d1c59d878d5eb35a0159580329df4626
Richard Eames <reames@asymmetricventures.com>
2014-08-21 13:02:18 -0600

    Use django-jinja if available

--- asymmetricbase/jinja/__init__.py
+++ asymmetricbase/jinja/__init__.py
@@ -17,23 +17,23 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from django.conf import settings
-from django.utils.functional import lazy, SimpleLazyObject
-import jinja2
-from jinja2.ext import WithExtension, LoopControlExtension
-
-from asymmetricbase.jinja.tags.csrf_token import CSRFTokenExtension
-
 from . import filters, global_functions, environment
-from .monkey_patching import monkey_patch_jinja
 
-def get_jinja_env():
+def get_asym_jinja_env():
+	from django.conf import settings
+	from django.utils.functional import lazy, SimpleLazyObject
 	from django.template.loaders.app_directories import	app_template_dirs
-	template_loader = getattr(settings, 'ASYM_TEMPLATE_LOADER', jinja2.FileSystemLoader(app_template_dirs))
 
+	import jinja2
+	from jinja2.ext import WithExtension, LoopControlExtension
+			
+	from asymmetricbase.jinja.tags.csrf_token import CSRFTokenExtension
+	from .monkey_patching import monkey_patch_jinja
+	template_loader = getattr(settings, 'ASYM_TEMPLATE_LOADER', jinja2.FileSystemLoader(app_template_dirs))
+	
 	if callable(template_loader):
 		template_loader = template_loader()
-
+	
 	jinja_env = environment.JinjaEnvironment(
 		loader = template_loader,
 		undefined = environment.UndefinedVar,
@@ -44,9 +44,23 @@ def get_jinja_env():
 			LoopControlExtension
 		],
 	)
+	jinja_env.djjj = False
+	return jinja_env
+
+def get_template_module(self, template_name, ctx = None):
+	return self.get_template(template_name).make_module(vars = ctx)
+
+def get_jinja_env():
+	try:
+		import types
+		from django_jinja.base import env as jinja_env
+		jinja_env.undefined = environment.UndefinedVar
+		jinja_env.djjj = True
+		jinja_env.get_template_module = types.MethodType(get_template_module, jinja_env)
+	except ImportError:
+		jinja_env = get_asym_jina_env()
 	
 	jinja_env.globals.update(global_functions.get_functions(jinja_env))
-	
 	jinja_env.filters.update(filters.get_filters(jinja_env))
 	
 	return jinja_env
--- asymmetricbase/jinja/response.py
+++ asymmetricbase/jinja/response.py
@@ -23,20 +23,23 @@ from django.template.context import RequestContext
 from asymmetricbase.jinja import jinja_env
 from asymmetricbase.logging import logger #@UnusedImport
 
-class JinjaTemplateResponse(TemplateResponse):
+if not jinja_env.djjj:
+	class JinjaTemplateResponse(TemplateResponse):
 	
-	def resolve_template(self, template):
-		if isinstance(template, (list, tuple)):
-			return jinja_env.select_template(template)
-		elif isinstance(template, basestring):
-			return jinja_env.get_template(template)
-		else:
-			return template
+		def resolve_template(self, template):
+			if isinstance(template, (list, tuple)):
+				return jinja_env.select_template(template)
+			elif isinstance(template, basestring):
+				return jinja_env.get_template(template)
+			else:
+				return template
 	
-	def resolve_context(self, context):
-		context = super(JinjaTemplateResponse, self).resolve_context(context)
+		def resolve_context(self, context):
+			context = super(JinjaTemplateResponse, self).resolve_context(context)
 		
-		if isinstance(context, RequestContext):
-			context = jinja_env.context_to_dict(context)
+			if isinstance(context, RequestContext):
+				context = jinja_env.context_to_dict(context)
 			
-		return context
+			return context
+else:
+	JinjaTemplateResponse = TemplateResponse
